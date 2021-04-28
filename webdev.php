60291bf611698d380e963cf4e5ccb3952ae93e0d
kaisar12012000 <71655666+kaisar12012000@users.noreply.github.com>
2021-04-28 15:58:05 +0530

    Add files via upload

---
+++ webdev.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="stylesheet" href="../DigitalMarketing/smm.css">
+        <title>Website Design and Development</title>
+        <style>
+          .dropbtn {
+            display: inline-block;
+            border: 2px solid #dcdcdc;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            color: #dcdcdc;
+          }
+
+          .dropdown a:hover, .dropdown:hover .dropbtn {
+            background-color: crimson;
+            color: #fff;
+          }
+
+          .dropdown {
+            display: inline-block;
+          }
+
+          .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+          }
+
+          .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+          }
+
+          .dropdown-content a:hover {background-color: #f1f1f1; color:#000;}
+
+          .dropdown:hover .dropdown-content {
+            display: block;
+          }
+          .imgs2 {
+            justify-content: space-around;
+            padding-top: 5px;
+          }
+        </style>
+    </head>
+    <body>
+        <!---------------------------------------------------------Navigation-------------------------------------------------------------->
+        <?php require_once "../../partials/navbar.php" ?>
+        <!--------------------------------------------------------Banner--------------------------------------------------------------->
+        <div class="banner text-center">
+            <h1 class="display-1">WEBSITE DESIGN AND DEVELOPMENT..</h1>
+            <p class="fs-3">The entire world is on the internet. How long will you relly on your old fashioned ways?</p>
+            <small class="fs-6" style="margin:60px">Join us today! Get help directly from Web Dev's...</small><br>
+            <a href="#" class="btn btn-dark" style="margin:60px 60px 0px; padding: 20px">Speak to our Web Dev's</a>
+        </div>
+        <!-----------------------------------------------------About Web Dev--------------------------------------------------------------->
+        <div class="container row container-1">
+            <div class="col-5">
+                <img src="https://images.pexels.com/photos/4506231/pexels-photo-4506231.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4506231.jpg&fm=jpg/" alt="SMM_imp">
+            </div>
+            <div class="col-1"></div>
+            <div class="col-6">
+                <h1 class="display-2">About Web Design/Dev Post 1...</h1>
+                <p class="text-justify dl-horizontal">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                     Quaerat, eveniet provident porro consectetur quae soluta minima beatae tenetur exercitationem
+                      tempore fugiat sunt earum ea, ratione quos ipsam! Ex, laboriosam recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                       Perspiciatis maiores quidem iusto in impedit nostrum voluptatem mollitia.
+                       Rem, reprehenderit corporis omnis quidem incidunt, ipsam nihil voluptatem soluta, impedit quo delectus!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                         Unde voluptates fugit porro suscipit eaque vero, ab earum id illo! Adipisci maxime sunt quis nihil saepe.
+                          Fuga ipsa dignissimos nesciunt. Molestias.
+                </p>
+                <p class="text-justify dl-horizontal">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                     Quaerat, eveniet provident porro consectetur quae soluta minima beatae tenetur exercitationem
+                      tempore fugiat sunt earum ea, ratione quos ipsam! Ex, laboriosam recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                       Perspiciatis maiores quidem iusto in impedit nostrum voluptatem mollitia.
+                </p>
+                <a href="#" class="btn btn-primary cont2_a">Get yourself a website now!</a>
+            </div>
+        </div>
+        <!-------------------------------------------------WWHYBAAS-------------------------------------------------------------------->
+        <div class="container container-2 text-center">
+            <h1 class="display-4 cont2_h1">Begin your career as a professional!!!</h1>
+            <p class="fs-5 cont2_p">We are an organization of inovative people who strive for the betterment of our clients.
+             We understand you and your needs and provide services accordingly. Join us today to experience a service one can only dream of.</p>
+             <small class="cont2_small">Here is everything you need to get assured of succes in digital marketing...</small>
+             <div class="row imgs">
+                <a href="#" class="col-2 rounded-circle img1">Web Content Creation</a>
+                <div class="col-1"></div>
+                <a href="#" class="col-2 rounded-circle img2">Web Designing</a>
+                <div class="col-1"></div>
+                <a href="#" class="col-2 rounded-circle img3">Full stack dev</a>
+                <div class="col-1"></div>
+                <a href="#" class="col-2 rounded-circle img4">Domain and DB</a>
+             </div>
+             <div class="row imgs2">
+                <a href="#" class="col-2 rounded-circle img1">Web apps</a>
+                <div class="col-1"></div>
+                <a href="#" class="col-2 rounded-circle img2">Trending functions</a>
+                <div class="col-1"></div>
+                <a href="#" class="col-2 rounded-circle img3">Full access on DB</a>
+                <div class="col-1"></div>
+                <a href="#" class="col-2 rounded-circle img4">You write zero code</a>
+             </div>
+        </div>
+        <!--------------------------------------------------------Clients--------------------------------------------------------------->
+        <div class="clients container">
+            <h1 class="display-2 text-center">Clients</h1>
+            <div class="row image-row">
+              <img src="https://raw.githubusercontent.com/anirudh-kac/CKY-Digital-Marketing-Site/main/Main/images/lodha.webp?token=ARCWB4WBLGR2ZHJ3Z224YCLASDBRQ" alt="client" class="col-3">
+              <img src="https://raw.githubusercontent.com/anirudh-kac/CKY-Digital-Marketing-Site/main/Main/images/lodha.webp?token=ARCWB4WBLGR2ZHJ3Z224YCLASDBRQ" alt="client" class="col-3">
+              <img src="https://raw.githubusercontent.com/anirudh-kac/CKY-Digital-Marketing-Site/main/Main/images/lodha.webp?token=ARCWB4WBLGR2ZHJ3Z224YCLASDBRQ" alt="client" class="col-3">
+              <img src="https://raw.githubusercontent.com/anirudh-kac/CKY-Digital-Marketing-Site/main/Main/images/lodha.webp?token=ARCWB4WBLGR2ZHJ3Z224YCLASDBRQ" alt="client" class="col-3">
+            </div>
+            <div class="row image-row">
+              <img src="https://raw.githubusercontent.com/anirudh-kac/CKY-Digital-Marketing-Site/main/Main/images/lodha.webp?token=ARCWB4WBLGR2ZHJ3Z224YCLASDBRQ" alt="client" class="col-3">
+              <img src="https://raw.githubusercontent.com/anirudh-kac/CKY-Digital-Marketing-Site/main/Main/images/lodha.webp?token=ARCWB4WBLGR2ZHJ3Z224YCLASDBRQ" alt="client" class="col-3">
+              <img src="https://raw.githubusercontent.com/anirudh-kac/CKY-Digital-Marketing-Site/main/Main/images/lodha.webp?token=ARCWB4WBLGR2ZHJ3Z224YCLASDBRQ" alt="client" class="col-3">
+              <img src="https://raw.githubusercontent.com/anirudh-kac/CKY-Digital-Marketing-Site/main/Main/images/lodha.webp?token=ARCWB4WBLGR2ZHJ3Z224YCLASDBRQ" alt="client" class="col-3">
+            </div>
+        </div>
+        <!--------------------------------------------------------Why Us?--------------------------------------------------------------->
+        <div class="container why_us row">
+          <div class="why_u2 col-6">
+            <h2 class="display-3 text-center">Why choose Us???</h2>
+            <p class="fs-5 justify-text">
+              We have a team of expert social media marketer who have an experience of more than six years. Who started just like you having very less
+              resources and some even had nothing. DMS-CKY helps yopu realize what resources you have and what are your potential. We encourage you to
+              believe in yourself and make the best off yor resources. We help you in improving your planning stratergies such that everytime you make 
+              an investment your resources increase.
+            </p>
+            <p class="fs-5 justify-text">
+              In a business such as yours or anyone's, at a stage like this even a profit of Rs 1 is considered to be a profit. We help you plan your 
+              investment cycle and make sure you make a good turn-over. You are getting help from young enthusiasts who strive for the economic and financial betterment of the 
+              society. Join us today to experience something you would only call as 7 star service.
+            </p>
+        </div>
+            <div class="row why_us1 col-6">
+              <div class="why_us1_p">
+                <p>Connect and influence more people in less time. Zero effort needed to to promote your buiness. Nearly 149 minutes per day investment.
+                Connect and influence more people in less time. Zero effort needed to to promote your buiness. Nearly 149 minutes per day investment.
+                </p>
+                <p>You are not alone. There are many with you. Nearly 81% of small businesses are doing this. You are not alone. There are many with you.
+                 Nearly 81% of small businesses are doing thisYou are not alone. There are many with you. Nearly 81% of small businesses are doing this.
+                 You are not alone. There are many with you. Nearly 81% of small businesses are doing this.You are not alone.</p>
+              </div>
+              <div class="why_us1_p">
+                <p>One social media plat form is not enough. You got to have atleast two. 91% of small retails own two social media accounts.
+                You are not alone. There are many with you. Nearly 81% of small businesses are doing this.
+                You are not alone. There are many with you. Nearly 81% of small businesses are doing this.</p>
+                <p>Social media businesses grew upto 328 million between October 2018 to October 2019.
+                One social media plat form is not enough. You got to have atleast two. 91% of small retails own two social media accounts.
+                One social media plat form is not enough. You got to have atleast two. 91% of small retails own two social media accounts.</p>
+              </div>
+            </div>
+        </div>
+        <!--------------------------------------------------------How can we help you?--------------------------------------------------------------->
+        <div class="container row how_can_we_help_you">
+            <div class="col-4 hcwhy">
+              <h3 class="display-4 hcwhy_content">How can we help you?</h3>
+              <h5 class="fs-1 hcwhy_content">Get a Quote for you..</h5>
+              <small class="fs-5 hcwhy_content">We ssually respond within 24 hours from your request.</small>
+            </div>
+            <div class="col-8 hcwhy">
+              <form action="" class="form-group hcwhy_content">
+                <div class="form_part1">
+                  <input type="text" class="hcwhy_content_textField" placeholder="Name*"><br>
+                  <input type="text" class="hcwhy_content_textField" placeholder="Mobile Number*"><br>
+                  <label class="fs-4 hcwhy_content">Services</label><br>
+                  <input class="form-check-input hcwhy_content" type="checkbox" value="" id="defaultCheck1">
+                  <label class="form-check-label hcwhy_content" for="defaultCheck1">Consultance about Digital stratergy</label><br>
+                  <input class="form-check-input hcwhy_content" type="checkbox" value="" id="defaultCheck1">
+                  <label class="form-check-label hcwhy_content" for="defaultCheck1">Creating online leads</label><br>
+                  <input class="form-check-input hcwhy_content" type="checkbox" value="" id="defaultCheck1">
+                  <label class="form-check-label hcwhy_content" for="defaultCheck1">Create digital platforms (Websites/Apps)</label><br>
+                  <input class="form-check-input hcwhy_content" type="checkbox" value="" id="defaultCheck1">
+                  <label class="form-check-label hcwhy_content" for="defaultCheck1">Social media management and marketing</label><br>
+                  <input class="form-check-input hcwhy_content" type="checkbox" value="" id="defaultCheck1">
+                  <label class="form-check-label hcwhy_content" for="defaultCheck1">Creative Campaign and Designing</label><br>
+                  <a href="#" class="btn cont2_a">Send us a Message</a>
+                </div>
+                <div class="form_part2 hcwhy_content">
+                  <input type="text" class="hcwhy_content_textField" placeholder="Enter valid Email*"><br>
+                  <input type="text" class="hcwhy_content_textField" placeholder="Name of the company*"><br>
+                  <div class="input-group">
+                    <div class="dropdown">
+                      <a href="javascript:void(0)" class="dropbtn" style="text-decoration: none; color: #000; folnt-size: 22px">Your turnover</a>
+                      <div class="dropdown-content">
+                        <a href="#">50k-3.5Lakh</a>
+                        <a href="#">3.5Lakh-7Lakh</a>
+                        <a href="#">7Lakh-10Lakh</a>
+                        <a href="#">Above 10Lakh</a>
+                      </div>
+                    </div>
+                    <input type="text" class="form-control hcwhy_content_dropdown" aria-label="Text input with dropdown button">
+                  </div>
+                </div>
+              </form>
+            </div>
+        </div>
+        <!--------------------------------------------------------FAQ--------------------------------------------------------------->
+        <div class="container faq text-center">
+            <h1 class="display-3 text-center">FREQUENTLY ASKED QUESTION(FAQ's)</h1>
+            <a href="#" class="btn text-center faq_a">Long Question Statement?</a><br>
+            <a href="#" class="btn text-center faq_a">Long Question Statement?</a><br>
+            <a href="#" class="btn text-center faq_a">Long Question Statement?</a><br>
+        </div>
+        <!--------------------------------------------------Footer---------------------------------------------------------------->
+        <?php require_once "../../partials/footer.php" ?>
+    </body>
+</html>
